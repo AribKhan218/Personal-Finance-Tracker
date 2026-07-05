d702d706a5ba18769a77af52af24f87d21975809
AribKhan218 <aribmakm218@gmail.com>
2026-07-05 19:47:13 +0500

    feat: functions:- totals, filter, search, renderTransactions

--- script.js
+++ script.js
@@ -12,11 +12,95 @@ const transactions = document.querySelector(".transactions");
 const transactionList = document.querySelector(".transaction-list");
 const actions = document.querySelector(".actions");
 const inputs = document.querySelectorAll("input");
+const InFilter = document.querySelector(".filter");
+const InSearch = document.querySelector(".search input");
 
 const AllTransactions = [];
-let currentBalance = 0;
-let totalIncome = 0;
-let totalExpense = 0;
+
+function renderTransactions(data = AllTransactions) {
+  transactionList.innerHTML = "";
+  // adding transactions to transaction-list
+  data.forEach((transaction) => {
+    const transactionHTML = `<div class="transaction">
+    <div>${transaction.description}</div>
+                                    <div>$${transaction.amount}</div>
+                                    <div>${transaction.type}</div>
+                                    <div>${transaction.category}</div>
+                                    <div>${transaction.timeStamps.replace("T", ", ")}</div>
+                                    <div class="actions">
+                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="edit" src="assets/pen.png" alt=""></div>
+                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="delete" src="assets/delete.png" alt=""></div>
+                                        </div>
+                                        </div>`;
+    transactionList.innerHTML += transactionHTML;
+  });
+}
+
+function calculateTotals() {
+  let currentBalance = 0;
+  let totalIncome = 0;
+  let totalExpense = 0;
+
+  for (const transaction of AllTransactions) {
+    // Total Income
+    if (transaction.type === "income") {
+      totalIncome += transaction.amount;
+      console.log(totalIncome);
+      Income.innerHTML = `$${totalIncome}`;
+    }
+    // Total Expense
+    if (transaction.type === "expense") {
+      totalExpense += transaction.amount;
+      console.log(totalExpense);
+      Expense.innerHTML = `$${totalExpense}`;
+    }
+  }
+  // Current Balance
+  currentBalance = totalIncome - totalExpense;
+  if (currentBalance >= 0) {
+    currBalance.innerHTML = `$${currentBalance}`;
+    currBalance.style.color = "#23c552";
+  } else if (currentBalance < 0) {
+    currBalance.innerHTML = `$${currentBalance}`;
+    currBalance.style.color = "#F84F31";
+  }
+}
+
+InFilter.addEventListener("change", (e) => {
+  filter();
+});
+
+function filter() {
+  let filteredArray;
+  let filter = InFilter.value.toLowerCase();
+  if (filter === "all") {
+    renderTransactions(AllTransactions);
+  } else if (filter === "expense") {
+    filteredArray = AllTransactions.filter((transaction) => {
+      return transaction.type === "expense";
+    });
+  } else if (filter === "income") {
+    filteredArray = AllTransactions.filter((transaction) => {
+      return transaction.type === "income";
+    });
+  }
+  renderTransactions(filteredArray);
+}
+
+InSearch.addEventListener("input", (e) => {
+  search();
+});
+
+function search() {
+  let searchedTransaction;
+  let search = InSearch.value.toLowerCase();
+  searchedTransaction = AllTransactions.filter((transaction) => {
+    if (transaction.description.toLowerCase().includes(search)) {
+      return true;
+    }
+  });
+  renderTransactions(searchedTransaction);
+}
 
 function addTransactions() {
   // getting values
@@ -26,7 +110,7 @@ function addTransactions() {
   const type = InType.value;
   const category = InCategory.value;
 
-  // displaying transactions
+  // displaying transactions UI
   noData.style.display = "none";
   transactions.style.display = "block";
 
@@ -46,28 +130,6 @@ function addTransactions() {
     category,
   };
 
-  // Total Income
-  if (transaction.type === "income") {
-    totalIncome += amount;
-    console.log(totalIncome);
-    Income.innerHTML = `$${totalIncome}`;
-  }
-  // Total Expense
-  if (transaction.type === "expense") {
-    totalExpense += amount;
-    console.log(totalExpense);
-    Expense.innerHTML = `$${totalExpense}`;
-  }
-  // Current Balance
-  currentBalance = totalIncome - totalExpense;
-  if (currentBalance >= 0) {
-    currBalance.innerHTML = `$${currentBalance}`;
-    currBalance.style.color = "#23c552";
-  } else if (currentBalance < 0) {
-    currBalance.innerHTML = `$${currentBalance}`;
-    currBalance.style.color = "#F84F31";
-  }
-
   // Check for duplicate values
   const isDuplicate = AllTransactions.some((t) => {
     return (
@@ -86,22 +148,8 @@ function addTransactions() {
 
   // adding transaction to AllTransaction
   AllTransactions.push(transaction);
-  transactionList.innerHTML = "";
-  // adding transactions to transaction-list
-  AllTransactions.forEach((transaction) => {
-    const transactionHTML = `<div class="transaction">
-      <div>${transaction.description}</div>
-                                    <div>$${transaction.amount}</div>
-                                    <div>${transaction.type}</div>
-                                    <div>${transaction.category}</div>
-                                    <div>${transaction.timeStamps.replace("T", ", ")}</div>
-                                    <div class="actions">
-                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="edit" src="assets/pen.png" alt=""></div>
-                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="delete" src="assets/delete.png" alt=""></div>
-                                    </div>
-                                    </div>`;
-    transactionList.innerHTML += transactionHTML;
-  });
+  calculateTotals();
+  renderTransactions();
 }
 
 addBtn.addEventListener("click", (e) => {
