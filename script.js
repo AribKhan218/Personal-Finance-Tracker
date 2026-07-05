25d566860a46bf62ade70a84ca845226cfef8662
AribKhan218 <aribmakm218@gmail.com>
2026-07-05 12:13:49 +0500

    JS Added - Feat: addTransactions() + UI tweaks

---
+++ script.js
@@ -0,0 +1,85 @@
+const currBalance = document.querySelector(".currBalance");
+const InDescription = document.querySelector(".desc");
+const InAmount = document.querySelector(".amount");
+const InTimeStamps = document.querySelector(".datetime");
+const InType = document.querySelector(".type");
+const InCategory = document.querySelector(".category");
+const addBtn = document.querySelector(".addBtn");
+const totalIncome = document.querySelector(".income");
+const totalExpense = document.querySelector(".expense");
+const noData = document.querySelector(".noData");
+const transactions = document.querySelector(".transactions");
+const transactionList = document.querySelector(".transaction-list");
+const actions = document.querySelector(".actions");
+const inputs = document.querySelectorAll("input");
+
+const AllTransactions = [];
+
+function addTransactions() {
+  // getting values
+  const description = InDescription.value;
+  const amount = InAmount.value;
+  const timeStamps = InTimeStamps.value;
+  const type = InType.value;
+  const category = InCategory.value;
+
+  // displaying transactions
+  noData.style.display = "none";
+  transactions.style.display = "block";
+
+  // Check for empty input values
+  if (description == "" || amount == "" || timeStamps == "") {
+    alert("Please fill all the inputs!");
+    return;
+  }
+
+  // transaction object
+  const transaction = {
+    // Object Shorthand
+    description,
+    amount,
+    timeStamps,
+    type,
+    category,
+  };
+
+  // Check for duplicate values
+  const isDuplicate = AllTransactions.some((t) => {
+    return (
+      t.description === transaction.description &&
+      t.amount === transaction.amount &&
+      t.timeStamps === transaction.timeStamps &&
+      t.type === transaction.type &&
+      t.category === transaction.category
+    );
+  });
+
+  if (isDuplicate) {
+    alert("An exact transaction already exists!");
+    return;
+  }
+
+  // adding transaction to AllTransaction
+  AllTransactions.push(transaction);
+  transactionList.innerHTML = "";
+  // adding transactions to transaction-list
+  AllTransactions.forEach((transaction) => {
+    const transactionHTML = `<div class="transaction">
+      <div>${transaction.description}</div>
+                                    <div>$${transaction.amount}</div>
+                                    <div>${transaction.type}</div>
+                                    <div>${transaction.category}</div>
+                                    <div>${transaction.timeStamps.replace("T", ", ")}</div>
+                                    <div class="actions">
+                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="edit" src="assets/pen.png" alt=""></div>
+                                        <div><img style="background-size: contain; width: 30px; height: 30px;" class="delete" src="assets/delete.png" alt=""></div>
+                                    </div>
+                                    </div>`;
+    transactionList.innerHTML += transactionHTML;
+  });
+}
+
+addBtn.addEventListener("click", (e) => {
+  addTransactions();
+  inputs.forEach((input) => (input.value = ""));
+});
